217e41beb96cf17f3a87552fada1826e1e8ba30d
guangzhi.wang <453089894@qq.com>
2016-07-13 19:50:26 +0800

    how to use it in project

---
+++ zknife-example/src/test/java/com/yudao/config/ExampleSettingsTest.java
@@ -0,0 +1,12 @@
+package com.yudao.config;
+
+public class ExampleSettingsTest {
+
+    public static void main(String[] args) {
+
+        ExampleSettings exampleSettings = ExampleSettings.Instance();
+        System.out.println(exampleSettings);
+
+    }
+
+}
